4976c14d19f6cd23c935f5f5b1b6d0c8fccdcd82
lander86 <40528366+lander86@users.noreply.github.com>
2023-03-07 20:37:39 +0100

    Implement yuma changes
    * implement yuma changes
    
    Co-authored-by: Yuri Sibirski <31861444+ysibirski@users.noreply.github.com>

--- README.md
+++ README.md
@@ -9,20 +9,11 @@ This project uses docker compose to spin up an evmapp node on Dune testnet.
 - pwgen
 
 ## Setup
-1. Populate the .env file starting from the .env.template:
+1. Set up environment variables in the .env 
     ```shell
-    cp .env.template .env
-    ```
-2. Require the following environment variables form Horizen Labs support team: 
-    ```shell
-    SCNODE_NET_KNOWNPEERS=
-    SCNODE_NET_MAGICBYTES=
-    ```
-3. Set up environment variables in the .env 
-    ```shell
-    SCNODE_NET_NODENAME=
-    SCNODE_WALLET_GENESIS_SECRETS= # These can be left empty
+    SCNODE_NET_NODENAME= # This must be populated with a proper value
     SCNODE_WALLET_SEED= # These can be left empty or use a random string
+    SCNODE_REST_PASSWORD= # Uncomment and set this variable only if you are willing to set up authentication on the rest api endpoints
     ```
 4. Run the following command to create the stack for the first time:
     ```shell
--- configs/entrypoint.sh
+++ configs/entrypoint.sh
@@ -42,9 +42,7 @@ to_check=(
   "SCNODE_CERT_SIGNERS_MAXPKS"
   "SCNODE_CERT_SIGNERS_PUBKEYS"
   "SCNODE_CERT_SIGNERS_THRESHOLD"
-  "SCNODE_CERT_SIGNING_ENABLED"
-  "SCNODE_CERT_SUBMITTER_ENABLED"
-  "SCNODE_FORGER_ENABLED"
+  "SCNODE_CERT_MASTERS_PUBKEYS"
   "SCNODE_FORGER_RESTRICT"
   "SCNODE_GENESIS_BLOCKHEX"
   "SCNODE_GENESIS_SCID"
@@ -53,12 +51,12 @@ to_check=(
   "SCNODE_GENESIS_MCNETWORK"
   "SCNODE_GENESIS_WITHDRAWALEPOCHLENGTH"
   "SCNODE_GENESIS_COMMTREEHASH"
+  "SCNODE_GENESIS_ISNONCEASING"
   "SCNODE_NET_DECLAREDADDRESS"
   "SCNODE_NET_KNOWNPEERS"
   "SCNODE_NET_MAGICBYTES"
   "SCNODE_NET_NODENAME"
   "SCNODE_NET_P2P_PORT"
-  "SCNODE_REST_PORT"
   "SCNODE_WALLET_SEED"
   "SCNODE_WALLET_MAXTX_FEE"
 )
@@ -71,20 +69,20 @@ for var in "${to_check[@]}"; do
 done
 
 if [ "${SCNODE_FORGER_RESTRICT:-}" = "true" ]; then
-  if [ -z "${SCNODE_FORGER_ALLOWED_FORGERS:-}" ]; then
-    echo "Error: Environment variable SCNODE_FORGER_ALLOWED_FORGERS required when SCNODE_FORGER_RESTRICT=true."
+  if [ -z "${SCNODE_ALLOWED_FORGERS:-}" ]; then
+    echo "Error: Environment variable SCNODE_ALLOWED_FORGERS required when SCNODE_FORGER_RESTRICT=true."
     sleep 5
     exit 1
   fi
 fi
 
-if [ "${SCNODE_CERT_SIGNING_ENABLED:-}" = "true" ] || [ "${SCNODE_CERT_SUBMITTER_ENABLED:-}" = "true" ]; then
-  if [ -z "${SCNODE_CERT_SIGNERS_SECRETS:-}" ]; then
-    echo "Error: Environment variable SCNODE_CERT_SIGNERS_SECRETS required when SCNODE_CERT_SIGNING_ENABLED=true or SCNODE_CERT_SUBMITTER_ENABLED=true."
-    sleep 5
-    exit 1
-  fi
+
+# set REST API password hash
+SCNODE_REST_APIKEYHASH=""
+if [ -n "${SCNODE_REST_PASSWORD:-}" ]; then
+  SCNODE_REST_APIKEYHASH="$(echo -en "\n        apiKeyHash = \"$(htpasswd -nbBC 10 "" "${SCNODE_REST_PASSWORD}" | tr -d ':\n' | sed 's/$2y/$2a/')\"")"
 fi
+export SCNODE_REST_APIKEYHASH
 
 # download and extract backup for import
 backupdir="/sidechain/datadir/backupStorage"
@@ -95,83 +93,25 @@ if [ -n "${SCNODE_BACKUP_TAR_GZ_URL:-}" ] && ! [ -f "${backupdir}/.import_done" 
   touch "${backupdir}/.import_done"
 fi
 
-# detect zend container IP, check zend is up and websocket port is reachable
-if [ -n "${SCNODE_WS_ZEN_FQDN:-}" ]; then
-  to_check=(
-    "RPC_PASSWORD"
-    "RPC_PORT"
-    "RPC_USER"
-    "SCNODE_WS_ZEN_PORT"
-  )
-  for var in "${to_check[@]}"; do
-    if [ -z "${!var:-}" ]; then
-      echo "Error: Environment variable ${var} required when SCNODE_WS_ZEN_FQDN is set."
-      sleep 5
-      exit 1
-    fi
-  done
-
-  # detect IP address
-  i=0
-  while true; do
-    SCNODE_WS_ZEN_IP="$(dig A +short "${SCNODE_WS_ZEN_FQDN}" | grep -v ";" || true)"
-    if [ -n "${SCNODE_WS_ZEN_IP:-}" ]; then
-      break
-    fi
-    sleep 5
-    i="$((i+1))"
-    if [ "$i" -gt 48 ]; then
-      echo "Error: Failed to detect IP address of '${SCNODE_WS_ZEN_FQDN}' container after 4 minutes."
-      exit 1
-    fi
-  done
-  export SCNODE_WS_ZEN_IP
-  echo "Detected IP address '${SCNODE_WS_ZEN_IP}' of '${SCNODE_WS_ZEN_FQDN}' container."
-
-  # make sure zend is up by checking RPC interface
-  i=0
-  while ! curl --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockcount", "params": [] }' -H 'content-type: text/plain;' "http://${RPC_USER}:${RPC_PASSWORD}@${SCNODE_WS_ZEN_IP}:${RPC_PORT}/" &> /dev/null; do
-    echo "Waiting for '${SCNODE_WS_ZEN_FQDN}' container to be ready."
-    sleep 5
-    i="$((i+1))"
-    if [ "$i" -gt 48 ]; then
-      echo "Error: '${SCNODE_WS_ZEN_FQDN}' container not ready after 4 minutes."
-      exit 1
-    fi
-  done
-  echo "RPC server of '${SCNODE_WS_ZEN_FQDN}' container ready."
-
-  # make sure websocket port is reachable
-  i=0
-  while ! nc -z "${SCNODE_WS_ZEN_IP}" "${SCNODE_WS_ZEN_PORT}" &> /dev/null; do
-    echo "Waiting for '${SCNODE_WS_ZEN_FQDN}' container websocket port to be ready."
-    sleep 5
-    i="$((i+1))"
-    if [ "$i" -gt 48 ]; then
-      echo "Error: '${SCNODE_WS_ZEN_FQDN}' container websocket port not ready after 4 minutes."
-      exit 1
-    fi
-  done
-  echo "Websocket server of '${SCNODE_WS_ZEN_FQDN}' container ready."
-fi
-
 # convert literal '\n' to newlines
 SCNODE_CERT_SIGNERS_PUBKEYS="$(echo -e "${SCNODE_CERT_SIGNERS_PUBKEYS:-}")"
 SCNODE_CERT_SIGNERS_SECRETS="$(echo -e "${SCNODE_CERT_SIGNERS_SECRETS:-}")"
-SCNODE_FORGER_ALLOWED_FORGERS="$(echo -e "${SCNODE_FORGER_ALLOWED_FORGERS:-}")"
+SCNODE_CERT_MASTERS_PUBKEYS="$(echo -e "${SCNODE_CERT_MASTERS_PUBKEYS:-}")"
+SCNODE_ALLOWED_FORGERS="$(echo -e "${SCNODE_ALLOWED_FORGERS:-}")"
 SCNODE_NET_KNOWNPEERS="$(echo -e "${SCNODE_NET_KNOWNPEERS:-}")"
 export SCNODE_CERT_SIGNERS_PUBKEYS
 export SCNODE_CERT_SIGNERS_SECRETS
-export SCNODE_FORGER_ALLOWED_FORGERS
+export SCNODE_CERT_MASTERS_PUBKEYS
+export SCNODE_ALLOWED_FORGERS
 export SCNODE_NET_KNOWNPEERS
 
 # substitute env vars in scnode config file
 # shellcheck disable=SC2016,SC2026
-SUBST='$SCNODE_CERT_SIGNERS_MAXPKS:$SCNODE_CERT_SIGNERS_PUBKEYS:$SCNODE_CERT_SIGNERS_SECRETS:$SCNODE_CERT_SIGNERS_THRESHOLD:$SCNODE_CERT_SIGNING_ENABLED:'\
-'$SCNODE_CERT_SUBMITTER_ENABLED:$SCNODE_GENESIS_BLOCKHEX:$SCNODE_GENESIS_SCID:$SCNODE_GENESIS_POWDATA:$SCNODE_GENESIS_MCBLOCKHEIGHT:$SCNODE_GENESIS_MCNETWORK:'\
-'$SCNODE_GENESIS_WITHDRAWALEPOCHLENGTH:$SCNODE_GENESIS_COMMTREEHASH:$SCNODE_FORGER_ALLOWED_FORGERS:$SCNODE_FORGER_ENABLED:$SCNODE_FORGER_RESTRICT:'\
-'$SCNODE_NET_DECLAREDADDRESS:$SCNODE_NET_KNOWNPEERS:$SCNODE_NET_MAGICBYTES:$SCNODE_NET_NODENAME:$SCNODE_NET_P2P_PORT:$SCNODE_REST_PORT:'\
-'$SCNODE_WALLET_GENESIS_SECRETS:$SCNODE_WALLET_SEED:$SCNODE_WALLET_MAXTX_FEE:$SCNODE_WS_ZEN_IP:$SCNODE_WS_ZEN_PORT'
+SUBST='$SCNODE_CERT_MASTERS_PUBKEYS:$SCNODE_CERT_SIGNERS_MAXPKS:$SCNODE_CERT_SIGNERS_PUBKEYS:$SCNODE_CERT_SIGNERS_SECRETS:$SCNODE_CERT_SIGNERS_THRESHOLD:'\
+'$SCNODE_GENESIS_BLOCKHEX:$SCNODE_GENESIS_SCID:$SCNODE_GENESIS_POWDATA:$SCNODE_GENESIS_MCBLOCKHEIGHT:$SCNODE_GENESIS_MCNETWORK:'\
+'$SCNODE_GENESIS_WITHDRAWALEPOCHLENGTH:$SCNODE_GENESIS_COMMTREEHASH:$SCNODE_GENESIS_ISNONCEASING:$SCNODE_ALLOWED_FORGERS:$SCNODE_FORGER_RESTRICT:'\
+'$SCNODE_NET_DECLAREDADDRESS:$SCNODE_NET_KNOWNPEERS:$SCNODE_NET_MAGICBYTES:$SCNODE_NET_NODENAME:$SCNODE_NET_P2P_PORT:$SCNODE_REST_APIKEYHASH:'\
+'$SCNODE_WALLET_GENESIS_SECRETS:$SCNODE_WALLET_MAXTX_FEE:$SCNODE_WALLET_SEED:$WS_ADDRESS:$MAX_INCOMING_CONNECTIONS:$MAX_OUTGOING_CONNECTIONS'
 export SUBST
 envsubst "${SUBST}" < /sidechain/config/sc_settings.conf.tmpl > /sidechain/config/sc_settings.conf
 unset SUBST
